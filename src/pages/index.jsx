3df9001de7576bdf4910740f0f26cb28471c8ce8
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-04-20 12:33:28 -0700

    :rotating_light: fix codes to solve the error from vercel - SHU-127

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,8 +1,6 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.scss';
 import { HomeLayout } from '@/components/Layout/HomeLayout';
-import MainVisual from '@/assets/home-mv.svg';
-import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -11,16 +9,11 @@ export default function Home() {
         <title>WELCOME TO RIMA LAND</title>
         <meta name="description" content="WELCOME TO RIMA LAND" />
       </Head>
-      <div className={styles.main}>
-        {/* <Image src={MainVisual} alt="Welcome to RIMA LAND" sizes="100vw" priority />  */}
-      </div>
-
+      <div className={styles.main}></div>
     </>
   );
 }
 
 Home.getLayout = function getLayout(page) {
-  return (
-    <HomeLayout>{page}</HomeLayout>
-  )
-}
+  return <HomeLayout>{page}</HomeLayout>;
+};
