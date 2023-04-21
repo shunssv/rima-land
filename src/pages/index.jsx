b6cdf974053321e3bcd6ef38a5986a39dca5d38d
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-04-21 12:54:33 -0700

    :lipstick: style mv area - SHU-128

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import styles from '@/styles/Home.module.scss';
 import { HomeLayout } from '@/components/Layout/HomeLayout';
+import mainVisual from '@/assets/home-mv.svg';
+import Image from 'next/image';
 
 export default function Home() {
   return (
@@ -9,7 +11,18 @@ export default function Home() {
         <title>WELCOME TO RIMA LAND</title>
         <meta name="description" content="WELCOME TO RIMA LAND" />
       </Head>
-      <div className={styles.main}></div>
+      <div className={styles.main}>
+        <Image src={mainVisual} alt="Welcome to RIMA LAND" sizes="100vw" className={styles.mainVisual} priority /> 
+
+        <div className={styles.maintenanceInner}>
+          <p className={styles.text}>
+            RIMA LANDはNiziU RIMAファンサイトです。 
+          </p>
+          <p className={styles.text}>
+            WithUにて運営しております。
+          </p>
+        </div>
+      </div>
     </>
   );
 }
