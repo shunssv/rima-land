fe200b9431d8802779680812e9e2488508cc39bf
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-09-04 23:53:51 -0700

    :sparkles: Code scroll-to-top button - RIM-2

--- src/components/Layout/HomeHeader/index.jsx
+++ src/components/Layout/HomeHeader/index.jsx
@@ -1,12 +1,19 @@
 import styles from './HomeHeader.module.scss';
 import Link from 'next/link';
+import { IoIosArrowDropupCircle } from 'react-icons/io';
 
 export function HomeHeader() {
   return (
-    <header className={styles.headerContainer}>
-      <h1 className={styles.logoWrapper}>
-        <Link href="/">Rima Land</Link>
-      </h1>
-    </header>
+    <>
+      <header className={styles.headerContainer}>
+        <h1 className={styles.logoWrapper}>
+          <Link href="/">Rima Land</Link>
+        </h1>
+      </header>
+
+      <Link href="#" className={styles.scrollToTop}>
+        <IoIosArrowDropupCircle className={styles.icon} />
+      </Link>
+    </>
   );
 }
