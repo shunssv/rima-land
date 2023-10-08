4b907b093fbd873f8542cc04870f7e55b92ccb2c
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-10-07 22:05:50 -0700

    :sparkles: Add scroll to top function - RIM-4

--- src/components/Layout/HomeHeader/index.jsx
+++ src/components/Layout/HomeHeader/index.jsx
@@ -1,19 +1,39 @@
 import styles from './HomeHeader.module.scss';
 import Link from 'next/link';
+import { useCallback } from 'react';
 import { IoIosArrowDropupCircle } from 'react-icons/io';
+import { useInView } from 'react-intersection-observer';
 
 export function HomeHeader() {
+
+  const [ref, inView] = useInView({
+    threshold: 0,
+  });
+
+  const handleClick = useCallback((e) => {
+    e.preventDefalt();
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, []);
+
   return (
     <>
-      <header className={styles.headerContainer}>
+      <header className={styles.headerContainer} ref={ref}>
         <h1 className={styles.logoWrapper}>
           <Link href="/">Rima Land</Link>
         </h1>
       </header>
-
-      <Link href="#" className={styles.scrollToTop}>
-        <IoIosArrowDropupCircle className={styles.icon} />
-      </Link>
+      {inView ? (
+        <Link href="#" className={styles.scrollToTop}>
+          <IoIosArrowDropupCircle className={styles.icon} />
+        </Link>
+      ) : (
+        <Link href="#" className={`${styles.scrollToTop} ${styles.appear}`} onClick={handleClick}>
+          <IoIosArrowDropupCircle className={styles.icon} />
+        </Link>
+      )}
     </>
   );
 }
