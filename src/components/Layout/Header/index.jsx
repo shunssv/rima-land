62cc564b7b7ac8552ab37cacad19818e600c6642
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-10-29 12:53:15 -0700

    :lipstick: Change font family on header - RIM-24

--- src/components/Layout/Header/index.jsx
+++ src/components/Layout/Header/index.jsx
@@ -1,7 +1,10 @@
 import { useInView } from 'react-intersection-observer';
 import styles from './Header.module.scss';
 import Link from 'next/link';
+import { Noto_Sans_Mono } from 'next/font/google';
 import { ScrollToTopButton } from '@/components/ScrollToTopButton';
+
+const NotoSansMono = Noto_Sans_Mono({ weight: '700', subsets: ['latin'] });
 
 export function Header() {
   const [ref, inView] = useInView({
@@ -11,8 +14,8 @@ export function Header() {
   return (
     <>
       <header className={styles.headerContainer} ref={ref}>
-        <h1 className={styles.logoWrapper}>
-          <Link href="/">Rima Land</Link>
+        <h1 className={`${styles.logoWrapper} ${NotoSansMono.className}`}>
+          <Link href="/">RIMA LAND</Link>
         </h1>
       </header>
       {inView ? (
