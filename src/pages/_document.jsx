61a30e1d6bbaae9e3746340c6d9f2e90f316eed0
s_kusakabe <s_kusakabe@stagegroup.jp>
2023-10-28 17:45:18 -0700

    :sparkles: Add common ogp texts for x - RIM-20

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -2,8 +2,11 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html lang="ja" prefix="og: http://ogp.me/ns#">
+      <Head>
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@RimaLandByWithU" />
+      </Head>
       <body>
         <Main />
         <NextScript />
